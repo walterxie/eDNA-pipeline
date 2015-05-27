bd4cc45b9eb05c2f26f3051e7d299c5c723e2658
Walter <@>
2015-05-27 15:19:04 +1200

    correct spelling

---
+++ R/createAllRarefactionTable.r
@@ -0,0 +1,66 @@
+library(vegan)
+library(Hmisc)
+
+replicate = 1
+threshold = 150
+levels = rep(c("gamma","alpha","beta"),3)
+qs = rep(0:2,each=3)
+
+# change config below
+sourcePath <- "~/svn/compevol/research/NZGenomicObservatory/Metabarcoding/R/Modules/"
+setwd(sourcePath)
+
+workingPath <- "~/Projects/NZGO/pilot2010/pipeline/"
+experiments <-  c("16S", "18S", "trnL", "ITS", "COI", "COI-spun") # only for cm file name and folder name   
+matrixNames <-  experiments # 
+
+stringBySubOrPlot <- "-by-subplot"
+
+source("init.R", local=TRUE)
+
+# get min size all sites for rdiversityTable each gene
+getMinSizeAllSites <- function(expId, otuThre) {
+    minSample = 99999
+
+	communityMatrix <- init(expId, otuThre, stringBySubOrPlot)
+	minSample <- min(minSample, min(rowSums(communityMatrix)))  		
+
+	sampleSize <- minSample#floor(minSample/100) * 100 # make sure subsampling run  
+	print(paste("otuThre =", otuThre, ", min sample size per site =", minSample, ", take sample size per site =", sampleSize)) 
+    
+    return (sampleSize)
+}
+
+otuThre=97
+
+# main
+for (expId in 1:length(experiments)) {	
+    print(paste("experiment = ", experiments[expId], ", matrixName = ", matrixNames[expId], sep=""))
+    	
+	# add "by-plot" to merge 2 nearby columns 
+	matrixName <- paste(matrixNames[expId], stringBySubOrPlot, sep = "")
+	
+	# use same sample size for all OTU threshold, but diff for each gene
+	sampleSize <- getMinSizeAllSites(expId, otuThre) 
+	
+	#sampleSize = 1100
+	print(paste(experiments[expId], " takes sampleSize = ", sampleSize, sep=""))  
+
+	print(paste("Rarefaction at otuThre =", otuThre))
+	communityMatrix <- init(expId, otuThre, stringBySubOrPlot)
+
+	source("TurnoverDist.R", local=TRUE)
+	# need to put on the top
+	source("SampleCounts.R", local=TRUE)
+
+	source("Diversities.R", local=TRUE)
+
+	source("RarefactionDiversitiesTable.R", local=TRUE)
+
+	# create rarefaction table file which is calculated by RarefactionDiversitiesTable.R
+	outputRFTable <- paste(workingPath, experiments[expId], "/", matrixName, "-", otuThre, "-rarefaction-table.csv", sep = "")
+	source("Rarefaction.R", local=TRUE) 
+} 
+
+
+
