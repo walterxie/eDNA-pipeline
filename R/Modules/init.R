5f3d876909c635243e3e11ebe6e78526c1c3e887
walterxie <dong.w.xie@gmail.com>
2015-10-22 00:12:36 +1300

    update init

--- R/Modules/init.R
+++ R/Modules/init.R
@@ -45,7 +45,7 @@ getPlot <- function(subplots, sep="-") {
 # expId = 1:6
 # isPlot determines to use which matrix file, by subplot or plot 
 # min2 = rmSingleton, whether remove all singletons
-getCommunityMatrixT <- function(expId, isPlot, min2) {
+getCommunityMatrix <- function(expId, isPlot, min2) {
   if(!exists("verbose")) verbose <- FALSE 
   
   n <- length(matrixNames)
@@ -67,23 +67,37 @@ getCommunityMatrixT <- function(expId, isPlot, min2) {
   if(verbose) 
     cat("\nUpload community matrix : ", ncol(communityMatrix), "columns,", nrow(communityMatrix), "rows, from", inputCM, "\n") 
   
-  # filter column first to avoid empty rows after columns remvoed
-  if(any(colSums(communityMatrix)== 0))
-    communityMatrix <- rmVectorFromCM(communityMatrix, vectorThr=0, MARGIN=2)
-  #stop("Invalid input: community matrix has empty samples !")
-  if(any(rowSums(communityMatrix)== 0))
-    communityMatrix <- rmVectorFromCM(communityMatrix, vectorThr=0, MARGIN=1)
-  #stop("Invalid input: community matrix has empty OTUs !")
-  
-  communityMatrixT <- transposeCM(communityMatrix)
-  
   if (min2) {
-    singletons <- which(colSums(communityMatrixT)==1)
+    singletons <- which(rowSums(communityMatrix)==1)
     if(verbose) 
       cat("Remove", length(singletons) ,"singletons from ", matrixName, " !\n")
-    communityMatrixT <- communityMatrixT[,-singletons]
+    communityMatrix <- communityMatrix[-singletons,]
     rm(singletons)		
   }
+}
+
+getCommunityMatrixT <- function(expId, isPlot, min2) {
+  if(!exists("verbose")) verbose <- FALSE 
+  
+  communityMatrix <- getCommunityMatrix(expId, isPlot, min2)
+  
+  # filter column first to avoid empty rows after columns remvoed if vectorThr>0
+  if(any(colSums(communityMatrix)== 0)) {
+    nc <- ncol(communityMatrix)
+    communityMatrix <- rmVectorFromCM(communityMatrix, vectorThr=0, MARGIN=2)
+    if(nc > ncol(communityMatrix)) 
+      cat("Warning: remove", nc-ncol(communityMatrix), "empty columns from community matrix.\n") 
+  #stop("Invalid input: community matrix has empty samples !")
+  }
+  if(any(rowSums(communityMatrix)== 0)) {
+    nr <- nrow(communityMatrix)
+    communityMatrix <- rmVectorFromCM(communityMatrix, vectorThr=0, MARGIN=1)
+    if(nr > nrow(communityMatrix)) 
+      cat("Warning: remove", nr-nrow(communityMatrix), "empty rows from community matrix.\n") 
+    #stop("Invalid input: community matrix has empty OTUs !")
+  }
+  
+  communityMatrixT <- transposeCM(communityMatrix)
   
   return(communityMatrixT)
 }
@@ -111,10 +125,10 @@ getRarefactionTable <- function(expId, isPlot, min2) {
 unclassTaxa <- c("Not assigned", "No hits", "cellular organisms", "root")
 # rankLevel: the taxa level in each bar
 # groupLevel: used to assign colour for each group, and must higher than rankLevel
-# belongTo: keep OTU rows contain given taxa belongTo, if NULL, keep all
-# return CM + rankLevel + groupLevel
-getTaxaAssgReads <- function(expId, min2, rankLevel, groupLevel, belongTo) {
-  if(missing(belongTo)) belongTo<-NULL
+# taxa.group: keep OTU rows contain given taxa group, if "all", keep all
+# return taxaAssgReads = CM + rankLevel + groupLevel
+getTaxaAssgReads <- function(expId, min2, rankLevel, groupLevel, taxa.group="all") {
+  if(missing(taxa.group)) taxa.group<-NULL
   
   cat("Create taxonomy assignment for", matrixNames[expId], ".\n")
   
@@ -134,19 +148,21 @@ getTaxaAssgReads <- function(expId, min2, rankLevel, groupLevel, belongTo) {
   # define unclassified
   taxaPaths[taxaPaths==unclassTaxa[1] | taxaPaths==unclassTaxa[2] | taxaPaths==unclassTaxa[3] | is.na(taxaPaths)] <- "unclassified"
   
+  ##### keep OTU rows contain given taxa belongTo ##### 
+  if (taxa.group == "assigned") {
+    taxaPaths <- taxaPaths[-which(grepl("unclassified", taxaPaths[,1])),]
+  } else if (taxa.group != "all") {
+    taxaPaths <- taxaPaths[which(grepl(taxa.group, taxaPaths[,1])),] # taxaPaths[,1] is taxa path separated by ;
+    #if (tolower(taxa.group) == "metazoa")  # non Arthropoda
+    #  taxaPaths <- taxaPaths[-which(grepl("Arthropoda", taxaPaths[,1])),]		
+  }
+  
+  ###### taxa assignment by reads #####
   if ( ! tolower(rankLevel) %in% tolower(colnames(taxaPaths)) ) 
     stop( paste("Column name", rankLevel, "not exist in taxa path file for ", matrixNames[expId]) )
   if (! tolower(groupLevel) %in% tolower(colnames(taxaPaths)) ) 
     stop( paste("Column name", groupLevel, "not exist in taxa path file for ", matrixNames[expId]) )
   
-  ##### keep OTU rows contain given taxa belongTo ##### 
-  if (!is.null(belongTo)) {
-    taxaPaths <- taxaPaths[which(grepl(belongTo, taxaPaths[,1])),] # taxaPaths[,1] is taxa path separated by ;
-    if (belongTo == "Metazoa")  # non Arthropoda
-      taxaPaths <- taxaPaths[-which(grepl("Arthropoda", taxaPaths[,1])),]		
-  }
-  
-  ###### taxa assignment by reads #####
   colRankLevel <- which(tolower(colnames(taxaPaths))==tolower(rankLevel))
   colGroupLevel <- which(tolower(colnames(taxaPaths))==tolower(groupLevel))
   
@@ -158,6 +174,7 @@ getTaxaAssgReads <- function(expId, min2, rankLevel, groupLevel, belongTo) {
   return(taxaAssgReads)
 }
 
+###### dissimilarity matrix #####
 # Dissimilarity matrix of paired samples
 # diss.fun = "beta1-1", "jaccard", "horn.morisita"
 getDissimilarityMatrix <- function(expId, isPlot, min2, diss.fun="beta1-1") {
@@ -177,27 +194,6 @@ getDissimilarityMatrix <- function(expId, isPlot, min2, diss.fun="beta1-1") {
   diss.matrix <- readFile(file=inputB, sep=",")
   
   return(diss.matrix)
-}
-
-
-######## beta1-1 #######
-getBeta1Minus1 <- function(expId, isPlot, min2) {
-  n <- length(matrixNames) 
-  matrixName <- matrixNames[expId]
-  # hard code for Vegetation that only has plot and always keep singletons
-  if (expId == n) {
-    matrixName <- postfix(matrixName, TRUE, FALSE, sep="-")
-  } else {
-    matrixName <- postfix(matrixName, isPlot, min2, sep="-") 
-  }
-  
-  inputB <- paste(workingPath, "data/", matrixName, "-beta1-1.csv", sep = "")
-  if(verbose) 
-    cat("\nUpload beta1-1 matrix : from", inputB, "\n") 
-  
-  beta1_1 <- readFile(file=inputB, sep=",")
-  
-  return(beta1_1)
 }
 
 
@@ -229,45 +225,4 @@ getElevPlotDist <- function(plot.names, env.plot) {
   return(dist(env.plot.match[,colElev]))
 }
 
-getElevSample <- function(cmRowNames) { 
-  colPlot = 1
-  colElev = 2
-  
-  sample_count <- length(cmRowNames)
-  print(paste("sample_count = ", sample_count, sep=""))
-  
-  inputElevation <- paste(workingPath, "data/plot_elevations.txt", sep="")	
-  elev <- read.table(inputElevation, header=TRUE, stringsAsFactors=FALSE)
-  
-  # keep elev for easy debug
-  elevTmp <- elev
-  
-  # hard code to fit in given data
-  if (sample_count <= 10) {
-    # 2 subplots within the plot should be next each other 
-    for (i in seq(1,nrow(elevTmp),by=2)) {
-      elevTmp[i,colPlot] <- unlist(strsplit(as.character(elevTmp[i,colPlot]), "-", fixed = TRUE))[1] # hard code for names Plot1-B
-      elevTmp[i+1,colPlot] <- unlist(strsplit(as.character(elevTmp[i+1,colPlot]), "-", fixed = TRUE))[1] 
-      if(elevTmp[i,colPlot]  != elevTmp[i+1,colPlot] ) stop("Two subplots within the same plot should be next each other in the meta data file !") 
-    }
-    
-    # map subplots meta data to plots
-    elevTmp <- elevTmp[-seq(1,nrow(elevTmp), by=2),]		
-  } 
-  
-  # missing data from traditional method
-  if (sample_count == 8) {
-    print(paste("Remove sample ", elevTmp[c(7,8),colPlot], " from meta data file due to miss data."))
-    elevTmp <- elevTmp[c(-7,-8),]
-  } 
-  
-  # remove not mapped samples from tmpEnvData
-  if ( all(cmRowNames != elevTmp[,colPlot]) ) {
-    stop(paste("Incorrect plots mapping between CM and meta data :  cm = ", cmRowNames, ", meta data =", elevTmp[,colPlot]))
-  } 
-  
-  elevPlot <- data.frame(row.names=cmRowNames)
-  elevPlot$Elevation.m <- elevTmp[,colElev]
-  return(elevPlot) # 2nd col Elevation.m
-}
 
