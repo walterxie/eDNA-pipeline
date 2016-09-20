32d26cea84d15787bd3587a254409f53313ef2d8
Walter <walterxie@users.noreply.github.com>
2016-09-20 12:27:29 +1200

    fix bug on taxa fig

--- R/allTaxonomyAnalyses.r
+++ R/allTaxonomyAnalyses.r
@@ -14,9 +14,10 @@ getAllCountsSums <- function(by.plot=TRUE, file.xtable=NULL, invalid.char=FALSE,
   require(ComMA)
   for (data.id in 1:length(input.names)) {
     # inlcude singletons
+    min2=FALSE
     cat("\n", output.names[data.id], "OTU clustering statistics,", ifelse(min2, "exclude", "include"), 
         "singletons, samples are based on", ifelse(by.plot, "plot", "subplot"), ".\n") 
-    cm <- getCommunityMatrix(input.names[data.id], min2=FALSE, by.plot=by.plot)
+    cm <- getCommunityMatrix(input.names[data.id], min2=min2, by.plot=by.plot)
     tt <- getTaxaTable(input.names[data.id], taxa.group="all")
     # adjust taxonomy?
     tt$kingdom <- gsub("CHROMISTA|PROTOZOA", "PROTISTS", tt$kingdom)
