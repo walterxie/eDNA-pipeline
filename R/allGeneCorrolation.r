fc7b8526c7cb16b6041e11b0e95ee887d35b0ce6
Walter <walterxie@users.noreply.github.com>
2016-12-16 17:14:00 +1300

    refactory heatmap

--- R/allGeneCorrolation.r
+++ R/allGeneCorrolation.r
@@ -38,28 +38,22 @@ getMantelAndProcrustes <- function(input.names, metric="jaccard",
 }
 
 # plotMantelAndProcrustes(corrs)
-plotMantelAndProcrustes <- function(corrs, text.repel=T, gene.levels=c("16S", "18S", "26S", "ITS", "COI-300", "COI-650")) {
+plotMantelAndProcrustes <- function(corrs, text.repel=T, label.digits=3, 
+                                    gene.levels=c("16S", "18S", "26S", "ITS", "COI-300", "COI-650")) {
   require(ggplot2)
   theme_set(theme_bw(base_size=8))
-  min.cor <- min(corrs$corrs[corrs$corrs>0])
-  cat("min correlation is ", min.cor, "max is ", max(corrs$corrs), "\n.")
-
+  
   m.p.tri <- corrs$corrs
-  m.p.tri$gene <- rownames(m.p.tri)
-  m.p.tri[m.p.tri==0] <- NA
+  p.hm <- ComMA::plotCombinedTriMatrix(m.p.tri, label.digits=label.digits, data.levels=gene.levels,
+                                       #lower.lim=0.58, upper.lim=1.0, breaks.by=0.1,
+                                       title="", legend.title="Correlations")
   
-  p.hm <- ComMA::ggHeatmap(m.p.tri, melt.id="gene", title="", legend.title="Correlations", 
-                        x.levels=gene.levels, y.levels=rev(gene.levels), label.digits=3,
-                        high = "#f46d43", mid = "#ffffbf", low = "#3288bd", 
-                        midpoint = mean(c(0.4, 1)), limit = c(0.4, 1), 
-                        breaks = c(0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0) )
-  
-  p.m.mds <- ComMA::ggNMDSPlot(dist(corrs$mantel.tri), text.or.point=1, text.size=5, text.repel=text.repel,
-                               title="Mantel test", title.add.stress=F, title.hjust=0.5) + 
+  p.m.mds <- ComMA::nmdsTriMatrix(dist(corrs$mantel.tri), text.or.point=1, text.size=5, 
+                                  text.repel=text.repel, title="Mantel test") + 
     expand_limits(x = c(-1, 1), y=c(-1, 1))
   
-  p.p.mds <- ComMA::ggNMDSPlot(dist(corrs$prot.tri), text.or.point=1, text.size=5, text.repel=text.repel,
-                               title="Procrustes test", title.add.stress=F, title.hjust=0.5) + 
+  p.p.mds <- ComMA::nmdsTriMatrix(dist(corrs$prot.tri), text.or.point=1, text.size=5, 
+                                  text.repel=text.repel, title="Procrustes test") + 
     expand_limits(x = c(-1, 1), y=c(-1, 1))
   
   list( heatmap=p.hm, mantel.mds=p.m.mds, prot.mds=p.p.mds, 
